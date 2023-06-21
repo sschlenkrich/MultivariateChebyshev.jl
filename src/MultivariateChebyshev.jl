ec808529f4fafa116568b58586faceae6bcc6c29
Sebastian Schlenkrich <sebastian@schlenkrich.de>
2023-06-21 07:27:39 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.38

---
+++ src/MultivariateChebyshev.jl
@@ -0,0 +1,5 @@
+module MultivariateChebyshev
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MultivariateChebyshev
+using Test
+
+@testset "MultivariateChebyshev.jl" begin
+    # Write your tests here.
+end
